cda3db8e5d063c2707040b2eec99baf803e616d5
Alex Davidson <alex.davidson@bluewire-technologies.com>
2016-08-03 17:36:17 +0100

    Enable parallelisation of tests

--- LargeCollections.Tests/Properties/AssemblyInfo.cs
+++ LargeCollections.Tests/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using NUnit.Framework;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -24,3 +25,5 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("9.2.0")]
 [assembly: AssemblyFileVersion("9.2.0")]
+
+[assembly: Parallelizable]
